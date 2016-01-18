a8c20b8ff295cbefb73ec3f37d252282a4d87f57
umesh <umesh936@gmail.com>
2016-01-18 18:32:16 +0530

    supporting quotes ' " '  in time comparsion

--- getLastFiveMinuteLogs.sh
+++ getLastFiveMinuteLogs.sh
@@ -3,5 +3,6 @@ endDate=$(date +"%d-%m-%y:%H:%M:%S")
 startDate=$(date +"%d-%m-%y:%H:%M:%S" --date='5 min ago')
 echo "start Date $startDate"
 echo "end Date $endDate"
-awk -v sD="$startDate" -v eD="$endDate" ' $1>=sD && $1<=eD  ' other_vhosts_access.log > /var/log/apache2/test
+#awk -v sD="$startDate" -v eD="$endDate" ' $1>=sD && $1<=eD  ' other_vhosts_access.log > /var/log/apache2/test
+awk   -v sD="$startDate" -v eD="$endDate" '   {  gsub(/\"/,"",$1); if($1>=sD && $1<=eD) { $1="\""$1"\""; print}}' other_vhosts_access.log > /var/log/apache2/currentLog.log
 
